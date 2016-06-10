944a7565bbfc85328c2071f32cf87e29ae1f2352
cronaline <cronaline@gmail.com>
2016-06-09 23:39:26 -0500

    Ejercicio 1.6 y 1.7 agregados

---
+++ Cap.1/Ej1.67.c
@@ -0,0 +1,9 @@
+//Ejercicio 1.6 y Ejercicio 1.7
+//Verifique que la expresion getchar() != EOF es 0 o 1
+#include <stdio.h>
+
+int main()
+{
+    printf("Valor de la expresion getchar() != EOF: %d\n", getchar() != EOF);
+    printf("Valor de EOF: %d\n", EOF);
+}
